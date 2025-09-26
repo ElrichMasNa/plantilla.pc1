045918a09473edb551a818f3beb1548ce7f64f68
jose saavedra <u20191c464@upc.edu.pe>
2025-09-26 12:32:53 -0500

    add PrimeVue components and configuration to main.js

---
+++ src/shared/presentation/languaje-switcher.vue
@@ -0,0 +1,14 @@
+﻿<script setup>
+
+import {SelectButton as PvSelectButton} from "primevue";
+</script>
+
+<template>
+<pv-select-button
+    v-model = "$i18n.locale" :options = "$i18n.availablelocales.map(locals => ({label: locale, toUpperCase: locals.toUpperCase(), value: locale}))"/>
+
+</template>
+
+<style scoped>
+
+</style>
