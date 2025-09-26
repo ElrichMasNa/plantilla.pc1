4d036d691bc5b8a3632afca93a3afbfd205b4583
jose saavedra <u20191c464@upc.edu.pe>
2025-09-26 12:29:00 -0500

    add PrimeVue components and configuration to main.js

--- src/main.js
+++ src/main.js
@@ -3,20 +3,22 @@ import './style.css'
 import App from './app.vue'
 import i18n from "./i18n.js";
 import PrimeVue from 'primevue/config';
-import material from '@primeuix/themes/material';
+import Material from '@primeuix/themes/material';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-import {button} from "@primeuix/themes/aura/inputnumber";
-import {Avatar, Button, Drawer, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
+import {Avatar, Button, Card, Drawer, Menu, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
+// noinspection JSCheckFunctionSignatures
 createApp(App)
     .use(i18n)
     .use(PrimeVue, {ripple: true, theme: {preset: Material}})
-    .component('pv-button', button)
+    .component('pv-button', Button)
     .component('pv-select-button', SelectButton)
     .component('pv-avatar', Avatar)
     .component('pv-drawer',  Drawer)
     .component('pv-image', Image)
     .component('pv-toolbar', Toolbar)
     .component('pv-menubar', Menubar)
+    .component('pv-card', Card)
+    .component('pv-menu', Menu)
     .directive('tooltip', Tooltip)
     .mount('#app')
